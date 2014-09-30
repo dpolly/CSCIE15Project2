c04ef9611060808b827865742ee738617a75ebdb
Denise Polly <dpolly@g.harvard.edu>
2014-09-29 20:41:27 -0500

    Initial File

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Password Generator</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+  <body>
+    <div id="header" class="row">
+		<?php include ("content/header.php"); ?>
+    </div>
+    <div id="content" class="row">
+        <?php require ("content/content.php"); ?>
+	</div>
+	<div id="footer" class="row">
+        <?php include ("content/footer.php"); ?>
+	</div>
+  </body>
+</html>
