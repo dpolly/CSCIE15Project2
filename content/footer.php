e70338fa8c49d4ca33a240bcd31c97d3a5a10c63
Denise Polly <dpolly@g.harvard.edu>
2014-09-30 22:44:29 -0500

    Updated footer

--- content/footer.php
+++ content/footer.php
@@ -1,6 +1,5 @@
 <div id="footer" class="small-12 columns">
-    <p> We’re stoked you want to try Foundation! To get going, this file (index.html) includes some basic styles you can modify, play around with, or totally destroy to get going.
-        Once you've exhausted the fun in this document, you should check out: Foundation Documentation Everything you need to know about using the framework. Foundation on Github Latest code, issue reports, feature requests and more.
-        @foundation zurb Ping us on Twitter if you have questions. If you build something with this we'd love to see it (and send you a totally boss sticker).
-    </p>
+    <div class="panel" >
+        <h6 align="center">CSCIE-15 Dynamic Web Applications Project 2</h6>
+    </div>
 </div>
