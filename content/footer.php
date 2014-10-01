aaea108d723769e7bf836807c394cc71c5b5bb9e
Denise Polly <dpolly@g.harvard.edu>
2014-09-30 19:28:56 -0500

    Initial File

---
+++ content/footer.php
@@ -0,0 +1,6 @@
+<div id="footer" class="small-12 columns">
+    <p> We’re stoked you want to try Foundation! To get going, this file (index.html) includes some basic styles you can modify, play around with, or totally destroy to get going.
+        Once you've exhausted the fun in this document, you should check out: Foundation Documentation Everything you need to know about using the framework. Foundation on Github Latest code, issue reports, feature requests and more.
+        @foundation zurb Ping us on Twitter if you have questions. If you build something with this we'd love to see it (and send you a totally boss sticker).
+    </p>
+</div>
