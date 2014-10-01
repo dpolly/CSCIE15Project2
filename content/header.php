3303a11fc20b94c8edb01625147dc3a92f700970
Denise Polly <dpolly@g.harvard.edu>
2014-09-30 23:25:50 -0500

    Uploading full Draft

---
+++ content/header.php
@@ -0,0 +1,9 @@
+<div class="small-3 columns">
+    <img src="img/logo.png">
+</div>
+<div class="small-9 columns">
+    <div class="panel">
+    <h1 align="center">XKCD Password Generator</h1>
+    </div>
+</div>
+
