f45d1ecc0e67b72e898fd5c24c7a75b88d508528
Denise Polly <dpolly@g.harvard.edu>
2014-09-30 22:40:46 -0500

    Updated options, messaging and error checking.

--- php/logic.php
+++ php/logic.php
@@ -1,28 +1,38 @@
 <?php
 /**
- * Created by PhpStorm.
  * User: d_polly
- * Date: 9/28/2014
- * Time: 9:53 PM
+ * Date: Created 9-28-2014 Last Updated 9-30-2014
+ *       9-29-2014 DMP: added Capital,Special Characters and Number Options
+ *       9-30-2014 DMP: added messaging and isset to clear errors on page load.
  */
 
-    $pwDictionary = array ("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten");
+    $pwDictionary = array ("art", "field","quickly","thrown","tired","share","pair","tomorrow","aware","colour",
+                            "writing","whenever","quietly","fool","forced","touched","smiling","taste","dog","spent",
+                            "steps","worst","legs","watched","ay","thee","eight","worthy","wrote","manners",
+                            "proceeded","frightened","somewhat","born","greatest","charge","degree","shame","places",
+                            "maam","could","tongue","according","box","wine","filled","servants","calling","fallen","supper");
     $pwSpecialChar = array ("!", "#", "$", "%","*", "?");
     $pwList = "";
+    $message = "";
 
-if (isset($_GET["optNumberWords"]) && $_GET["optNumberWords"] > 0)
+if (isset($_GET["optNumberWords"]) && $_GET["optNumberWords"] > 0 && $_GET["optNumberWords"] <= 5)
 {
-    for ($i = 1; $i <= $_GET["optNumberWords"]; $i++) {
-        $tempWord = $pwDictionary[rand(0, 9)];
-        if (isset($_GET["optCapitalize"]) && $i = 1) $tempWord = ucfirst($tempWord);
-        $pwList = $pwList . $tempWord . " ";
+    for ($i = 1; $i <= $_GET["optNumberWords"]; $i++)
+    {
+        $tempWord = $pwDictionary[rand(0, 49)];
+        if ($_GET["optNumberWords"] > 1 && $i < $_GET["optNumberWords"]) $pwList = $pwList . $tempWord . " ";
+        else $pwList = $pwList.$tempWord;
     }
-    if (isset ($_GET["optNumberIncluded"])) $pwList = $pwList.rand(1, 9);
-    if (isset ($_GET["optSpecialChar"])) $pwList = $pwList. $pwSpecialChar[rand(0, 5)];
-    $pwList="Your new Password is.....".$pwList;
-    $_GET["optNumberWords"]="";
+    if (isset($_GET["optCapitalize"])) $pwList = ucfirst($pwList);
+    if (isset ($_GET["optNumberIncluded"])) $pwList = $pwList.rand(1, 50);
+    if (isset ($_GET["optSpecialChar"])) $pwList = $pwList.$pwSpecialChar[rand(0, 5)];
+    $message="Your new password is.....";
+}
+else if (isset($_GET["optNumberWords"]) && $_GET["optNumberWords"] > 5)
+{
+    $message="Sorry! maximum of 5 words allowed";
 }
 else
 {
-    $pwList = "";
+    $message="";
 }
